a513090117a19fab9d6927186e3ac4ff2d3f15f9
Velin-Todorov <velintodorov@yahoo.com>
2023-06-03 20:24:22 +0200

    css changes

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,17 +1,56 @@
+import type { User } from "@clerk/nextjs/api";
+import { clerkClient } from "@clerk/nextjs";
 import { z } from "zod";
 import {
   createTRPCRouter,
   publicProcedure,
   protectedProcedure,
 } from "~/server/api/trpc";
+import { TRPCError } from "@trpc/server";
+
+
+const filterUserForClient = (user: User) => {
+  return {
+    id: user.id,
+    name: user.username,
+    profilePic: user.profileImageUrl
+  }
+}
 
 export const postsRouter = createTRPCRouter({
   
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+
+    const posts = await ctx.prisma.post.findMany({
+      take: 100
+    })
+
+    const users =( await clerkClient.users.getUserList({
+      userId: posts.map((post) => post.authorId),
+      limit: 100,
+
+    })).map(filterUserForClient)
+    
+    return posts.map((post) => {
+      const author =  users.find((user) => user.id === post.authorId);
+      
+      if (!author || !author.name ) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Author for post not found"
+        });
+      }
+  
+      return {
+        post,
+        author,
+      };
+    })
   }),
 
   getSecretMessage: protectedProcedure.query(() => {
     return "you can now see this secret message!";
-  }),
+  })
 });
+
+  
