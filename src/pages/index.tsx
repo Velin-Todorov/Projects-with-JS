19698b733d0b6e7004ee471c93222d7f157a26ef
Velin-Todorov <velintodorov@yahoo.com>
2023-06-03 14:47:20 +0200

    changes on the sign in button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,6 +16,9 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+        <div>
+          <SignIn />
+        </div>
         <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
       </main>
     </>
