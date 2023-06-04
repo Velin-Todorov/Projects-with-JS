3bed0cbf3c9d81ce7737f65041c2505816e5f235
Velin-Todorov <velintodorov@yahoo.com>
2023-06-04 17:26:03 +0200

    font sizing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -41,7 +41,7 @@ const PostView = (props: PostWithUser) => {
 					<span className="font-thin">{`  - ${dayjs(post.createdAt).fromNow()}`}</span>
 				</div>
 				<div className="flex p-2">
-					<span>
+					<span className="text-2xl">
 						{post.content}
 					</span>
 				</div>
